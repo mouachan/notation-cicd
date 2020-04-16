1244f997c3d6ad9291e2c35063e532f09dc04ccd
mouachan <mourad.ouachani@gmail.com>
2020-04-16 18:12:44 +0200

    quarkus build container added

--- jenkinsFile-notation-svc.groovy
+++ jenkinsFile-notation-svc.groovy
@@ -1,13 +1,13 @@
-  // define commands
-  def mvnCmd = "mvnw"
-  // injection of environment variables is not done so set them here...
-  def sourceRef = "master"
-  def sourceUrl = "https://github.com/mouachan/credit"
-  def githubId = "github"
-  def devProject = "credit"
-  def artifact = "dmn-svc-notation"
-  def version = "1.0.0"
-  def contextDirPath = "notation/dmn-svc-notation"
+// define commands
+def mvnCmd = "./mvnw"
+// injection of environment variables is not done so set them here...
+def sourceRef = "master"
+def sourceUrl = "https://github.com/mouachan/credit"
+def githubId = "github"
+def devProject = "credit"
+def artifact = "dmn-svc-notation"
+def version = "1.0.0"
+def contextDirPath = "notation/dmn-svc-notation"
 // namespaces
 def namespace_dev = "notation-svc-dev"
 def namespace_acp = "notation-svc-acp"
@@ -26,7 +26,7 @@ node('maven') {
     sh "${mvnCmd} test"
   stage 'deployInDev'
     echo "building container image"
-    sh "${mvnCmd} fabric8:build -Dmaven.test.skip -Dimage.version=latest -Dfabric8.namespace=${namespace_dev}"
+    sh "${mvnCmd} clean package -Dquarkus.container-image.build=true"
     sh "oc process template/dmn-svc-template-template -n notation-cicd --param APP_NAME=${artifact} --param APP_VERSION=${version} --param IMAGE_VERSION=${image_version} | oc apply -n ${namespace_dev} -f -"
     echo "promoting image"
 	  sh "oc tag ${namespace_dev}/${artifact}:latest ${namespace_acp}/${artifact}:${image_version}"
