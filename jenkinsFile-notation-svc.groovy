dc43065493444c14219d16b8d529ca5a913d6c80
mouachan <mourad.ouachani@gmail.com>
2020-04-20 20:54:24 +0200

    debugging

--- jenkinsFile-notation-svc.groovy
+++ jenkinsFile-notation-svc.groovy
@@ -20,12 +20,12 @@ node('maven') {
   stage 'checkout'
      git branch: sourceRef, url: sourceUrl, credentialsId: githubId
   stage 'build'
-     sh "${mvnCmd} clean install -DskipTests=true"
+    # sh "${mvnCmd} clean install -DskipTests=true"
   stage 'test'
-    sh "${mvnCmd} test"
+   # sh "${mvnCmd} test"
   stage 'deployInDev'
     echo "building container image"
-    sh "${mvnCmd} clean package -Dquarkus.kubernetes.deploy=true"
+    sh "${mvnCmd} clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.kubernetes.deployment-target=openshift -Dquarkus.container-image.registry=image-registry.openshift-image-registry.svc:5000 -Dquarkus.container-image.build=true -Dquarkus.container-image.push=true"
     sh "oc process -f ./template/dmn-svc.yml  --param APP_NAME=${artifact} --param APP_VERSION=${version} --param IMAGE_VERSION=${image_version} | oc apply -n ${namespace_dev} -f -"
     echo "promoting image"
 	  sh "oc tag ${namespace_dev}/${artifact}:${version} ${namespace_acp}/${artifact}:${image_version}"
